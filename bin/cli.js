74285d1e58e25b0d029603cf59be750da10393df
Aaron Throckmorton <aaron@throckmortons.com>
2019-02-06 16:17:23 -0700

    Create sample cli script

---
+++ bin/cli.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk')
+const program = require('commander')
+
+const [,, ...args] = process.argv
+
+const version = require('../package.json').version
+
+program
+  .version(require('../package.json').version, '-v, --version')
+  .option('-c, --config <configfile>', 'Use a specific config file')
+
+program
+  .command('list [directory]')
+  .description('List files and folders')
+  .option('-a, --all', 'List all files and folders')
+  .option('-l --long', '')
+  .action();
+
+
+  // .usage('<command> [options]')
+
+program.parse(process.argv)
+
+console.log(`Hello World v${version} - ${args}`)
